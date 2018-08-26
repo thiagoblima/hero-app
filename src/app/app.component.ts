89504273567b8b892bb5ac03090ca4566eafda30
Thiago Lima <thitheguy@gmail.com>
2018-08-26 19:56:27 -0300

    Adding to README.md the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  public pageTitle;
+  private pageTitle;
   constructor() {
   }
 
-  public set name (value: string) {
+  private set name (value: string) {
     this.pageTitle = value;
   }
 
